0c6d0cc015ae79a90898ba4f982463852c7f60a6
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-06-16 15:15:22 -0600

    feature done, user can create a new chatroom from the homepage

--- runtalk-chat/app/controllers/chats_controller.rb
+++ runtalk-chat/app/controllers/chats_controller.rb
@@ -6,4 +6,13 @@ class ChatsController < ApplicationController
   def show
     @chat = Chat.find_by_slug(params[:id])
   end
+
+  def new
+    @chat = Chat.new
+  end
+
+  def create
+    @chat = Chat.create!(params[:chat])
+    redirect_to chat_path(@chat)
+  end
 end
--- runtalk-chat/spec/features/chats_spec.rb
+++ runtalk-chat/spec/features/chats_spec.rb
@@ -4,6 +4,35 @@ describe Chat do
   let(:valid_name){"Valid Name"}
   let(:valid_slug){"validslug"}
   let(:chat){Chat.create!(name: valid_name, slug: valid_slug)}
+
+  describe 'creating a new chatroom' do 
+
+    context 'given the chatroom does not exist' do 
+
+      it 'creates a new chatroom' do 
+
+        visit '/'
+        click_on('Create New Chatroom')
+        fill_in 'Name', :with => "Example123"
+        fill_in 'Slug', :with => "example123"
+        click_button 'Create Chatroom'
+
+        expect(current_path).to eq "/chats/example123"
+        expect(page).to have_content("Example123")
+      end
+
+      it 'lists the chatroom as an available room'
+    end
+
+    context 'given the chatroom does exist' do 
+    
+      it 'informs the user that the chatroom already exists'
+
+    end
+
+  end
+
+
   describe 'visiting a chatroom' do 
     it 'should be accessible at /chats/:chat_slug' do 
       
