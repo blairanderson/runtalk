f539695855c277099462eae4838880b5ae1e9adf
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-06-18 07:24:10 -0600

    added pending specs for user model

--- runtalk-chat/spec/models/user_spec.rb
+++ runtalk-chat/spec/models/user_spec.rb
@@ -1,5 +1,78 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe ".username" do 
+
+    context "given a username exists" do 
+
+      it "does not create the user"
+
+      it "has an error on username"
+    end
+
+    context "a user does not give a username" do 
+
+      it "does not create the user"
+
+      it "has an error on username"
+    end
+
+    context "a user does give a username" do 
+
+      it "creates the user"
+    end
+  end
+
+  describe ".password" do 
+
+    context "a user does not give a password" do 
+
+      it "does not create the user"
+
+      it "has an error on password"
+    end
+
+    context "a user does give a password" do 
+
+      it "creates the user"
+    end
+  end
+
+  describe ".password_confirmation" do 
+
+    context "a user does not give a password confirmation" do 
+
+      it "does not create the user"
+
+      it "has an error on password confirmation"
+    end
+
+    context "a user does give a password confirmation" do 
+
+      it "creates the user"
+    end
+  end
+
+  describe ".email" do 
+
+    context "given an email exists" do 
+
+      it "does not create the user"
+
+      it "has an error on email"
+    end
+
+    context "a user does not give an email" do 
+
+      it "does not create the user"
+
+      it "has an error on email"
+    end
+
+    context "a user does give an email" do 
+
+      it "creates the user"
+    end
+  end
 end
