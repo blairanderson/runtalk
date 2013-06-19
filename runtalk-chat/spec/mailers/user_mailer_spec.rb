bb5b6e7cd798b770caafbaeffb53223abc99b501
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-06-19 17:15:59 -0600

    added mailer specs

--- runtalk-chat/spec/mailers/user_mailer_spec.rb
+++ runtalk-chat/spec/mailers/user_mailer_spec.rb
@@ -4,9 +4,18 @@ describe UserMailer do
 
   describe ".user_invitation" do 
 
-    it "sends an individual a confirmation email" do 
+    it "sends an individual an invitation email" do 
       mail = UserMailer.send_invitation("yolo@example.com")
       expect(mail.to).to eq ["yolo@example.com"]
+    end
+  end
+
+  describe ".send_registered_user_invitation" do 
+
+    it "sends a registered user an invitation email" do 
+      user = User.create(username: "yo", email: "yolo@example.com", password: "a_password")
+      mail = UserMailer.send_registered_user_invitation(user.email)
+      expect(mail.to).to eq [user.email]
     end
   end
 end
