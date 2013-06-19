7e633d4ee83166c6aa43c24ac4c623220e0486b9
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-06-19 14:37:21 -0600

    added pending specs for user notification email

---
+++ runtalk-chat/spec/features/user_invites_unregistered_user_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe User do 
+
+  describe "a registered user invites another individual" do 
+
+    context "the individual is a registered user" do 
+
+      it "allows access for that individual" 
+
+      it "sends an email notifying the user" 
+    end
+
+    context "the individual is not a registered user" do 
+
+      it "sends an email notifying the user"
+    end
+  end
+end
