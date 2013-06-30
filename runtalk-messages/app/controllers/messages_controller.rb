ba3b2ae22c478ef9913a9e333434dded990bcb3c
Blair Anderson <blair81@gmail.com>
2013-06-30 13:39:42 -0600

    optimizing message query in runtalk-messages controller

--- runtalk-messages/app/controllers/messages_controller.rb
+++ runtalk-messages/app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
 
   def index
-    @messages = Message.where(chat_id: params[:chat_id] ).last(20)
+    @messages = Message.where(chat_id: params[:chat_id] ).includes([:location, :photo]).last(20)
     render :json => @messages.to_json(include: [:location, :photo])
   end
 end
