710e17900144d38bac3c2d2d8d2ee12f17486428
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-07-02 07:56:43 -0600

    created runtalk invitations app

---
+++ runtalk-invitations/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RuntalkInvitations::Application.config.secret_key_base = '558b0f0a5552140503837085beb851b207fec6e1f51f494858316468c57210d8b6b03aebf5d9745e99a2ab5eecdfb5ef20f011db25423251a22aa58c81b32fc4'
---
+++ runtalk-invitations/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RuntalkInvitations::Application.config.session_store :cookie_store, key: '_runtalk-invitations_session'
