a0ac0335ea9132c10f5bdefdc5e6280890286257
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-07-02 11:17:38 -0600

    texts now working

--- runtalk-chat/app/models/invitation_proxy.rb
+++ runtalk-chat/app/models/invitation_proxy.rb
@@ -3,8 +3,8 @@ class InvitationProxy
   attr_reader :phone_number, :chat_id, :user_id
 
   def initialize(params)
-    @phone_number = params[:phone_number], 
-    @chat_id = params[:chat_id], 
+    @phone_number = params[:phone_number]
+    @chat_id = params[:chat_id]
     @user_id = params[:user_id]
   end
 
